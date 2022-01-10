ba781585aad700f8c2640a9356b89c9f78a266d4
Maykel Arias Torres <hanibal920915@gmail.com>
2022-01-10 11:29:43 -0500

    [Seccion 4] Terminado todas las pruebas

--- Seccion 4/count-app/src/PrimeraApp.js
+++ Seccion 4/count-app/src/PrimeraApp.js
@@ -1,23 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import React, { Fragment } from 'react';
 
-const PrimeraApp = ({ saludo, subtitulo }) => {
-  return (
-    <>
-      <h1> {saludo} </h1>
-      {/* <pre> {  JSON.stringify( saludo, null, 3 )  } </pre> */}
-      <p> {subtitulo} </p>
-    </>
-  );
+const PrimeraApp = ({saludo, subtitulo}) => {
+    return (
+        <>
+            <h1>{saludo}</h1>
+            <p>{subtitulo}</p>
+        </>
+    );
 };
 
 PrimeraApp.propTypes = {
-  saludo: PropTypes.string.isRequired,
+    saludo: PropTypes.string.isRequired,
 };
 
 PrimeraApp.defaultProps = {
-  subtitulo: "Soy un subtitulo",
+    subtitulo: "Soy un subtitulo",
 };
 
 export default PrimeraApp;
