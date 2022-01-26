a39a214859a5f0974d541275b92ce9947190aa50
Maykel Arias Torres <hanibal920915@gmail.com>
2022-01-25 21:10:21 -0500

    Agregado .gitignore

--- Seccion 6/gitexpertapp/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "animate.css";
-import { GifExpertApp } from "./GifExpertApp";
-
-ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
